b2d9226ec1c1299b5a094a5c6a1788f49b0fe94d
Tasnif Emran <tasnifemran@gmail.com>
2026-08-07 23:29:19 +0600

    configServerInitialized

---
+++ config-server/src/main/java/com/desco/config/ConfigServerApplication.java
@@ -0,0 +1,20 @@
+package com.desco.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+}
+
+
+/*DESCO Simulation - Config Server
+Central configuration server for all microservices.
+Serves configuration from the classpath configs/ directory.
+Protected by HTTP Basic Auth.*/
